5449100b43f99fbb519f3e6d34259765d7543542
Kosteksal <kosteks@outlook.com>
2022-05-02 18:02:56 +0300

    queue

--- src/queue.js
+++ src/queue.js
@@ -1,6 +1,6 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { ListNode } = require('../extensions/list-node.js');
+const { ListNode } = require('../extensions/list-node.js');
 
 /**
  * Implement the Queue with a given interface via linked list (use ListNode extension above).
@@ -13,23 +13,39 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.dequeue(); // returns the top element from queue and deletes it, returns 1
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
-class Queue {
 
-  getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+ 
 
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+ class Queue {
 
-  dequeue() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
-}
+  constructor() {
+    this.queue = null;
+};
+
+getUnderlyingList() {
+    return this.queue;
+};
+
+enqueue(value) {
+    if (!this.queue) {
+        this.queue = new ListNode(value);
+    } else {
+        let current = this.queue;
+        while (current.next) {
+            current = current.next;
+        };
+        current.next = new ListNode(value)
+    };
+};
+
+dequeue() {
+    let value =  this.queue.value;
+    this.queue = this.queue.next;
+    return value;
+};
+
+};
+
 
 module.exports = {
   Queue
--- src/stack.js
+++ src/stack.js
@@ -13,21 +13,30 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+  constructor () {
 
-  pop() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+    this.stack = [];
 
-  peek() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
-}
+};
+
+push(el) {
+
+    this.stack.push(el);
+
+};
+
+pop() {
+
+    return this.stack.pop();
+
+};
+
+peek() {
+
+    return this.stack[this.stack.length - 1]
+
+};
+};
 
 module.exports = {
   Stack
